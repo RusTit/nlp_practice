14ef925667d9bb4bb7ab71f0b3ffdf3f54ea4874
Russkih Anatoly <titanofdarkness@gmail.com>
2021-02-25 16:25:17 +1000

    Minor.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,19 @@ module.exports = {
   env: {
     es6: true,
     node: true,
-    mocha: true
+    mocha: true,
   },
-  extends: [
-    "airbnb-base",
-    "plugin:prettier/recommended",
-  ],
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-    document: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    document: 'readonly',
   },
   parserOptions: {
-    ecmaVersion: 2018
+    ecmaVersion: 2018,
   },
-  rules: {},
-  plugins: []
+  rules: {
+    'no-console': 'off',
+  },
+  plugins: [],
 };
