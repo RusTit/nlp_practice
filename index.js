ec3ed26d6500582377defc76e68604a6defeed7a
Russkih Anatoly <titanofdarkness@gmail.com>
2021-03-08 11:42:55 +1000

    Add some comments to the code.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const { ConsoleConnector } = require('@nlpjs/console-connector');
 const { dockStart } = require('@nlpjs/basic');
 const { exec, spawn } = require('child_process');
 
+// Helper function that convert common Nodejs app/command invoker into Promise
+// based. Promise will finished only AFTER app/command will finish
 const runCommand = async command => {
   return new Promise((resolve, reject) => {
     exec(command, (error, stdout, stderr) => {
@@ -16,6 +18,8 @@ const runCommand = async command => {
   });
 };
 
+// Run command or app and WAIT until it finished. While waiting,
+// this script cannot process new input and run others commands
 async function runCommandHandler(command) {
   try {
     console.log(await runCommand(command));
@@ -24,6 +28,8 @@ async function runCommandHandler(command) {
   }
 }
 
+// Run app or command, but DON't wait until it finished.
+// The app output will be in same console, so potentially can generate some mess
 function runCommandWithoutWaitingHandler(command, args = []) {
   try {
     const childProcess = spawn(command, args);
@@ -44,16 +50,25 @@ function runCommandWithoutWaitingHandler(command, args = []) {
   }
 }
 
+// App entry point
 const main = async () => {
+  // create nlp engine
   const dock = await dockStart();
+  // select nlp from the settings (conf.json)
   const nlp = dock.get('nlp');
+  // train selected nlp (the data will be used according to conf.json -> corpus-en.json)
   await nlp.train();
 
+  // Helper for running script as user console app. Allow to get user input from the terminal
   const connector = new ConsoleConnector();
+  // Event-function will be invoked each time user type the text and press Enter
   connector.onHear = async (self, text) => {
     self.say(`You said "${text}"`);
+    // Find the most suitable command for the user raw text
     const response = await nlp.process('en', text);
     console.log(`NLP answer: ${response.answer}`);
+    // Check the match object and run/execute related app/command/stuff
+    // intent(s) are described in the corpus-en.json
     switch (response.intent) {
       case 'run.browser':
         runCommandWithoutWaitingHandler('chromium');
