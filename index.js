d924580aff659b16d31a54296715282ea3bb4185
Russkih Anatoly <titanofdarkness@gmail.com>
2021-03-30 10:50:06 +1000

    Some more logic about the app

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { SlackAdapter } = require('botbuilder-adapter-slack');
 const { Botkit } = require('botkit');
+const { dockStart } = require('@nlpjs/basic');
 
 require('dotenv').config(); // load env vars from `.env` file
 
@@ -20,14 +21,38 @@ const adapter = new SlackAdapter({
   verificationToken: VERIFICATION_TOKEN,
 });
 
-const controller = new Botkit({
-  adapter,
-  // ...other options
-});
+const main = async () => {
+  // create nlp engine
+  const dock = await dockStart();
+  // select nlp from the settings (conf.json)
+  const nlp = dock.get('nlp');
+  // train selected nlp (the data will be used according to conf.json -> corpus-en.json)
+  await nlp.train();
 
-controller.on('message', async (bot, message) => {
-  await bot.reply(
-    message,
-    `I heard a message! text: ${message.text}, value: ${message.value}`
-  );
+  const controller = new Botkit({
+    adapter,
+    // ...other options
+  });
+
+  controller.on('message', async (bot, message) => {
+    const { text } = message;
+
+    if (text) {
+      // Find the most suitable command for the user raw text
+      const response = await nlp.process('en', text);
+      await bot.reply(
+        message,
+        `I heard a message! text: ${message.text}, value: ${message.value}, NLP answer: ${response.answer}`
+      );
+    } else {
+      await bot.reply(
+        message,
+        `Strange text: ${message.text}, value: ${message.value}`
+      );
+    }
+  });
+};
+
+main().catch(e => {
+  throw e;
 });
