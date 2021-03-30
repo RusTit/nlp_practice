1c83d7e50b8a60afc01a0147f2679368009410df
Russkih Anatoly <titanofdarkness@gmail.com>
2021-03-30 10:26:10 +1000

    Initial on botkit.

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const { dockStart } = require('@nlpjs/basic');
+const { SlackAdapter } = require('botbuilder-adapter-slack');
+const { Botkit } = require('botkit');
 
-(async () => {
-  const dock = await dockStart();
-})();
+// https://botkit.ai/docs/v4/reference/slack.html#SlackAdapterOptions
+
+const adapter = new SlackAdapter({
+  clientSigningSecret: process.env.SLACK_SECRET,
+  botToken: process.env.SLACK_TOKEN,
+});
+
+const controller = new Botkit({
+  adapter,
+  // ...other options
+});
+
+controller.on('message', async (bot, message) => {
+  await bot.reply(
+    message,
+    `I heard a message! text: ${message.text}, value: ${message.value}`
+  );
+});
