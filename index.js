ce20b40dc016ebf3f1527579fdf7807062630f92
Russkih Anatoly <titanofdarkness@gmail.com>
2021-03-30 10:50:31 +1000

    Some more logic about the app

--- index.js
+++ index.js
@@ -12,15 +12,6 @@ const {
   BOT_TOKEN,
 } = process.env;
 
-// https://botkit.ai/docs/v4/reference/slack.html#SlackAdapterOptions
-const adapter = new SlackAdapter({
-  clientId: CLIENT_ID,
-  clientSecret: CLIENT_SECRET,
-  botToken: BOT_TOKEN,
-  clientSigningSecret: CLIENT_SIGNING_SECRET,
-  verificationToken: VERIFICATION_TOKEN,
-});
-
 const main = async () => {
   // create nlp engine
   const dock = await dockStart();
@@ -28,6 +19,15 @@ const main = async () => {
   const nlp = dock.get('nlp');
   // train selected nlp (the data will be used according to conf.json -> corpus-en.json)
   await nlp.train();
+
+  // https://botkit.ai/docs/v4/reference/slack.html#SlackAdapterOptions
+  const adapter = new SlackAdapter({
+    clientId: CLIENT_ID,
+    clientSecret: CLIENT_SECRET,
+    botToken: BOT_TOKEN,
+    clientSigningSecret: CLIENT_SIGNING_SECRET,
+    verificationToken: VERIFICATION_TOKEN,
+  });
 
   const controller = new Botkit({
     adapter,
