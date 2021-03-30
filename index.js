418914b2c99adce756a3480e5759379b657034aa
Russkih Anatoly <titanofdarkness@gmail.com>
2021-03-30 10:28:06 +1000

    Initial on botkit.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const { SlackAdapter } = require('botbuilder-adapter-slack');
 const { Botkit } = require('botkit');
 
-// https://botkit.ai/docs/v4/reference/slack.html#SlackAdapterOptions
+require('dotenv').config(); // load env vars from `.env` file
 
+// https://botkit.ai/docs/v4/reference/slack.html#SlackAdapterOptions
 const adapter = new SlackAdapter({
   clientSigningSecret: process.env.SLACK_SECRET,
   botToken: process.env.SLACK_TOKEN,
